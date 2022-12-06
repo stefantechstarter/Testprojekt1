b9841e385f19e0a3c7192c06833a020d89e12a5c
Stefan Gaugel <stefan.gaugel@boschrexroth.de>
2022-12-06 14:59:25 +0100

    code2.javaupdate

--- Code/Code2.java
+++ Code/Code2.java
@@ -2,6 +2,6 @@ public class test {
 
       public static void main(String[] args) {
             // Ausgabe Hello World!
-            System.out.println("Hello Universe!");
+            System.out.println("Hello Universe63!");
       }
 }
